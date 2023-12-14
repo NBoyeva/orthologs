35b7fd187bf42309f87aad3812926ef19885834c
Nadzeya <boyevanadzeya@gmail.com>
2023-12-14 15:06:35 +0300

    Add translation filtration
    Check if sequence has stop-codons inside.
    
    Join paths with os.path.join.

--- filter_dev.py
+++ filter_dev.py
@@ -11,7 +11,7 @@ Takes a directory with fasta files isoforms in which to be filtered,
 yeilds the output directory with filtered files.
 
 Example of usage:
-cmd>>> python3 filter.py -i ./input_data_dir/ -o ./output_data_dir/
+cmd>>> python3 filter.py -i ./input_data_dir -o ./output_data_dir
 '''
 
 # Defining command line arguments
@@ -33,37 +33,45 @@ files = os.listdir(data_dir)
 
 for file in files:
 	print(f'Processing file: {file}') # notify about processing start
-	records = list(SeqIO.parse(open(data_dir + file), 'fasta'))
+	records = list(SeqIO.parse(open(os.path.join(data_dir, file)), 'fasta'))
 	regex = re.compile("\[gene=[\w]*\]")  # \w: [a-zA-Z0-9_]
 	
-	gene_names = [] # a list to track if we already met specific gene name (length equals to the number of unique gene names)
-	names = [] # a list to build filter list further (length equals to the number of records)
+	# a list to track if we already met specific gene name (length equals to the number of unique gene names)
+	gene_names = []
+	# a list to build filter list further (length equals to the number of records) 
+	names = [] 
 
-	for record in records:
-		gene = regex.search(record.description)
+	for record in records: # records are all records from fasta file 
+		gene = regex.search(record.description) 
 		
-		if gene is not None: 
-			gene = gene.group()
-			gene_name = gene[gene.find('=')+1: -1] # extract name of gene
+		sq = record.seq[:-1]
+		if '*' not in sq:
+		
+			if gene is not None: 
+				gene = gene.group()
+				gene_name = gene[gene.find('=')+1: -1] # extract name of gene
+				
+				# If the gene name has not been encountered yet
+				if gene_name not in gene_names: 
+					gene_names.append(gene_name)
+					names.append(gene_name)
+					
+				# If gene name had already been encountered:
+				else: 
+					length1 = len(records[names.index(gene_name)].seq) # check the length of the previous isoform
+					
+					# If the length of current isoform if bigger than it of the previous one:
+					if len(record.seq) > length1:
+						names[names.index(gene_name)] = 'drop this' # delete gene name from the first isoform position
+						names.append(gene_name) # add gene name for current isoform position
+					else:
+						names.append('drop this')
 			
-			# If the gene name has not been encountered yet
-			if gene_name not in gene_names: 
-				gene_names.append(gene_name)
-				names.append(gene_name)
-				
-			# If gene name had already been encountered:
-			else: 
-				length1 = len(records[names.index(gene_name)].seq) # check the length of the previous isoform
-				
-				# If the length of current isoform if bigger than it of the previous one:
-				if len(record.seq) > length1:
-					names[names.index(gene_name)] = 'drop this' # delete gene name from the first isoform position
-					names.append(gene_name) # add gene name for current isoform position
-				else:
-					names.append('drop this')
-		
-		else: # in case record does not have a gene name
-			names.append('no name')
+			else: # in case record does not have a gene name
+				names.append('no name')
+
+		else: # we drop sequences which were not translated correctly (have stop-codons)
+			names.append('drop this')
 
 	filter_list = [x != 'drop this' for x in names] # create a boolean list, where False is for record to be dropped and True is for record to be left
 	filtered_records = list(compress(records, filter_list)) # drop certain records
